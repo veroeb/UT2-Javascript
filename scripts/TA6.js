b09abe9230e532297d6d59546452293e8a8c2cc1
Veronica <vecheza@gmail.com>
2024-08-17 02:31:32 -0300

    TA6

---
+++ scripts/TA6.js
@@ -0,0 +1,11 @@
+const convertToCelsius = (temp) => {
+    console.log((temp - 30)/2);
+    
+};
+
+const convertToFahrenheit = (temp) => {
+    console.log((temp * 2) + 30);
+};
+
+convertToCelsius(80);
+convertToFahrenheit(20);
