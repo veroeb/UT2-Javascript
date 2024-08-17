7702aa2dfd73ef0bcf1833369038774499bbe48b
Veronica <vecheza@gmail.com>
2024-08-17 02:57:42 -0300

    Update TA9.js

--- scripts/TA9.js
+++ scripts/TA9.js
@@ -2,5 +2,7 @@ const getOdds = (nums) => {
     console.log(nums.filter((num) => num % 2 != 0));
 };
 
+const getOdds1 = (nums) => console.log(nums.filter((num) => num % 2 != 0));
+
 const array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-getOdds(array);
+getOdds1(array);
